29a9244c312e0d5efb9c86b8c462c4c54d3fcbe6
KiranBabu007 <kiranbabu3355@gmail.com>
2024-12-06 12:31:20 +0530

    Add workspaces configuration and update dependencies in package files

--- my-app/utils/db.js
+++ my-app/utils/db.js
@@ -1,10 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env.local
-dotenv.config({ path: './.env.local' });
 
 const sql = neon(process.env.NEXT_PUBLIC_POSTGRES_URL);
-export const db = drizzle({client:sql}, { schema });
+export const db = drizzle({ client: sql });
